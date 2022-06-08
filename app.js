5a16b6cbd30f2b0e077304c6ebaa82b1365fd678
Michelle Clement <michellelclement@gmail.com>
2022-06-08 12:37:08 +0000

    Additional amend to leave bubble animation

--- app.js
+++ app.js
@@ -25,7 +25,13 @@ const leaveAnimation = (current, done) => {
     tlLeave.fromTo(
       circles,
       { y: 0, opacity: 1 },
-      { y: -200, opacity: 0, stagger: 0.15 },
+      {
+        y: -200,
+        opacity: 0,
+        stagger: 0.15,
+        ease: "back.out(1.7)",
+        duration: 1
+      },
       "<"
     )
   );
