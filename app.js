6518fc79ea4aad7d3ee9c7da09fdea6a0dde6cbc
Michelle Clement <michellelclement@gmail.com>
2022-06-08 12:34:45 +0000

    Bubble leave animation sorted

--- app.js
+++ app.js
@@ -21,7 +21,13 @@ const leaveAnimation = (current, done) => {
       { y: 100, opacity: 0, onComplete: done },
       "<"
     ),
-    tlLeave.fromTo(text, { y: 0, opacity: 1 }, { opacity: 0, y: 100 }, "<")
+    tlLeave.fromTo(text, { y: 0, opacity: 1 }, { opacity: 0, y: 100 }, "<"),
+    tlLeave.fromTo(
+      circles,
+      { y: 0, opacity: 1 },
+      { y: -200, opacity: 0, stagger: 0.15 },
+      "<"
+    )
   );
 };
 
