b6595b59b7510f2a68e9e6d063886dcf1eed016a
Michelle Clement <michellelclement@gmail.com>
2022-06-09 12:50:36 +0000

    Start product transition. 1/2

--- app.js
+++ app.js
@@ -92,10 +92,37 @@ barba.init({
         let gradient = getGradient(data.next.namespace);
         enterAnimation(next, done, gradient);
       }
+    },
+    //Product page animation
+    {
+      name: "product-transition",
+      from: { namespace: ["handbag"] },
+      to: { namespace: ["product"] },
+      enter(data) {
+        const done = this.async();
+        let next = data.next.container;
+        productEnterAnimation(next, done);
+      },
+      leave(data) {
+        const done = this.async();
+        let current = data.current.container;
+        productLeaveAnimation(current, done);
+      }
     }
   ]
 });
 
+function productEnterAnimation(next, done) {
+  tlEnter.fromTo(next, { y: "100%" }, { y: "0%" });
+  tlEnter.fromTo(
+    ".card",
+    { opacity: 0, y: 50 },
+    { opacity: 1, y: 0, stagger: 0.1, onComplete: done }
+  );
+}
+function productLeaveAnimation(current, done) {
+  tlLeave.fromTo(current, { opacity: 1 }, { opacity: 0, onComplete: done });
+}
 //Changing gradient on showcase
 function getGradient(name) {
   switch (name) {
